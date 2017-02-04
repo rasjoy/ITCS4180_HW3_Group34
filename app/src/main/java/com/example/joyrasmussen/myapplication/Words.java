85eed194e5f1dde3c04d2c163cf7b4eb159841dd
rasjoy <rasmussenjoy@gmail.com>
2017-02-04 12:45:47 -0500

    Made the scroll view dynamically changed based on screensize

--- app/src/main/java/com/example/joyrasmussen/myapplication/Words.java
+++ app/src/main/java/com/example/joyrasmussen/myapplication/Words.java
@@ -1,7 +1,12 @@
 package com.example.joyrasmussen.myapplication;
 
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class Words extends AppCompatActivity {
 
@@ -13,7 +18,17 @@ public class Words extends AppCompatActivity {
         //getting screen size so that we can dynamically set the size of the scroll view.
 
 
+        ScrollView scrollView = (ScrollView) findViewById(R.id.wordScroll);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layoutWord);
 
+       /* for(int i = 0; i < 30; i++){
+            TextView text = new TextView(this);
+            text.setTextSize(16);
+            text.setText(i +"");
+           layout.addView(text);
+        }*/
+
+        
 
     }
 }
